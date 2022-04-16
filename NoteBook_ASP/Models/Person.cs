1fa7807b6f94415f4bd4e14f0d68113a6173c11b
Сергей Воробьев <kazan1003@gmail.com>
2022-04-16 11:13:31 +0300

    in work

---
+++ NoteBook_ASP/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace NoteBook_ASP.Models
+{
+    public class Person
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string SurName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string FullName
+        {
+            get { return SurName + " " + Name + " " + LastName; }
+        }
+    }
+}
--- NoteBook_ASP/Program.cs
+++ NoteBook_ASP/Program.cs
@@ -1,6 +1,158 @@
-var builder = WebApplication.CreateBuilder(args);
+using NoteBook_ASP.Models;
+using System.Text.RegularExpressions;
+
+// начальные данные
+List<Person> persons = new()
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom1", SurName = "Waits1", LastName = "Ivanich1", PhoneNumber = "111", Address = "adr1", Description = "descr1"},
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom2", SurName = "Waits2", LastName = "Ivanich2", PhoneNumber = "222", Address = "adr2", Description = "descr2"},
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom3", SurName = "Waits3", LastName = "Ivanich3", PhoneNumber = "333", Address = "adr3", Description = "descr3"}
+};
+
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
+    //string expressionForNumber = "^/api/users/([0 - 9]+)$";   // если id представл€ет число
+
+    // 2e752824-1657-4c7f-844b-6ec2e168e99c
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+    if (path == "/api/users" && request.Method == "GET")
+    {
+        await GetAllPeople(response);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
+    {
+        // получаем id из адреса url
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response, request);
+    }
+    else if (path == "/api/users" && request.Method == "POST")
+    {
+        await CreatePerson(response, request);
+    }
+    else if (path == "/api/users" && request.Method == "PUT")
+    {
+        await UpdatePerson(response, request);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "DELETE")
+    {
+        string? id = path.Value?.Split("/")[3];
+        await DeletePerson(id, response, request);
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+});
 
 app.Run();
+
+// получение всех пользователей
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(persons);
+}
+// получение одного пользовател€ по id
+async Task GetPerson(string? id, HttpResponse response, HttpRequest request)
+{
+    // получаем пользовател€ по id
+    Person? person = persons.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, отправл€ем его
+    if (person != null)
+        await response.WriteAsJsonAsync(person);
+    // если не найден, отправл€ем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "ѕользователь не найден" });
+    }
+}
+
+async Task DeletePerson(string? id, HttpResponse response, HttpRequest request)
+{
+    // получаем пользовател€ по id
+    Person? person = persons.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, удал€ем его
+    if (person != null)
+    {
+        persons.Remove(person);
+        await response.WriteAsJsonAsync(person);
+    }
+    // если не найден, отправл€ем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "ѕользователь не найден" });
+    }
+}
+
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользовател€
+        var person = await request.ReadFromJsonAsync<Person>();
+        if (person != null)
+        {
+            // устанавливаем id дл€ нового пользовател€
+            person.Id = Guid.NewGuid().ToString();
+            // добавл€ем пользовател€ в список
+            persons.Add(person);
+            await response.WriteAsJsonAsync(person);
+        }
+        else
+        {
+            throw new Exception("Ќекорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Ќекорректные данные" });
+    }
+}
+
+async Task UpdatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользовател€
+        Person? personData = await request.ReadFromJsonAsync<Person>();
+        if (personData != null)
+        {
+            // получаем пользовател€ по id
+            var person = persons.FirstOrDefault(u => u.Id == personData.Id);
+            // если пользователь найден, измен€ем его данные и отправл€ем обратно клиенту
+            if (person != null)
+            {
+                person.Name = personData.Name;
+                person.SurName = personData.SurName;
+                person.LastName = personData.LastName;
+                person.PhoneNumber = personData.PhoneNumber;
+                person.Address = personData.Address;
+                person.Description = personData.Description;
+                await response.WriteAsJsonAsync(person);
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new { message = "ѕользователь не найден" });
+            }
+        }
+        else
+        {
+            throw new Exception("Ќекорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Ќекорректные данные" });
+    }
+}
